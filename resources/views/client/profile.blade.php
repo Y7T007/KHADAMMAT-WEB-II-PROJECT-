17d6ec7300024e5194fc7be96a6aef9a22518b88
Y7T007 <yassir7t@gmail.com>
2024-04-19 21:37:46 +0100

    Fixed image Profil Overflow

--- resources/views/client/profile.blade.php
+++ resources/views/client/profile.blade.php
@@ -87,7 +87,7 @@
     <label for="image">Image</label>
     <div class="custom-file">
     @if($client->profile_picture)
-        <img style="border-radius: 50%; width: 200px; margin-left: 30px; margin-top: 10px; margin-bottom: 20px;" src="{{ Storage::url($client->profile_picture) }}" alt="Photo de profil">
+        <img style="border-radius: 50%; height:100px;width: 100px; margin-left: 30px; margin-top: 10px; margin-bottom: 20px;" src="{{ Storage::url($client->profile_picture) }}" alt="Photo de profil">
     @else
         <p>Aucune image de profil disponible</p>
     @endif
