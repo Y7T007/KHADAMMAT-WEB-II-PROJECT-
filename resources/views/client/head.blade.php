62c82c0a8a50f7d97e5e61de033fca0915206f04
Y7T007 <yassir7t@gmail.com>
2024-04-19 03:20:25 +0100

    All icons setted up and the bootstrapIconsLibrary is installed successfully

--- resources/views/client/head.blade.php
+++ resources/views/client/head.blade.php
@@ -14,6 +14,7 @@
     <link href="../Partenaire/assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
     <!-- Custom CSS -->
     <link href="../Partenaire/dist/css/style.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
