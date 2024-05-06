244a9cb45839535f3b9da1cff258b629fb3db377
Y7T007 <yassir7t@gmail.com>
2024-05-06 08:05:15 +0100

    Done : Feed is working successfully

--- resources/views/client/feed.blade.php
+++ resources/views/client/feed.blade.php
@@ -74,77 +74,60 @@
                                 <div class="title-div" style="width: auto;margin-top: 30px;margin-right: auto;margin-left: auto;margin-bottom: 30px;text-align: center;">
                                     <h1 style="text-align: center;">Fil D'actualité</h1>
                                 </div>
-                                <div style="height: auto;background: var(--bs-light-border-subtle);margin-right: auto;margin-left: auto;max-width: 899px;border-radius: 38px;">
+                                @foreach($demandedServices as $demandedService)
+                                    <div style="height: auto;background: var(--bs-light-border-subtle);margin-right: auto;margin-left: auto;max-width: 600px;border-radius: 38px;">
 
 
-                                    <div class="col">
-                                        <div class="row d-flex flex-row justify-content-center align-items-center align-content-center align-self-center" style="height: 80px;background: #3c3c3c;border-radius: 75px;width: 104%;margin-left: -2%;">
-                                            <div class="col-xl-1" style="max-width: 80px;min-width: 80px;"><img width="100" height="80" src="assets/img/office-front.jpeg" style="height: 60px;width: 60px;border-radius: 60px;margin-left: -1px;"></div>
-                                            <div class="col" style="margin-top: auto;margin-bottom: auto;">
-                                                <p style="font-size: 15px;font-family: Poppins;margin-top: auto;margin-bottom: auto;color: var(--bs-body-bg);"> <strong>Yassir WAHID</strong> (<i>#Client</i>)  -&gt;&nbsp;<strong>Nossair SEDKI</strong> (<i>#Partener</i>)</p>
-                                            </div>
-                                        </div>
-                                        <div class="row" style="width: auto;margin-right: 0;margin-left: 0;">
-                                            <div class="col">
-                                                <p style="text-align: left;font-size: 25px;height: auto;width: auto;padding: 43px;">M. <strong>Yassir WAHID</strong>&nbsp; a demander le service de "<strong>Netoyage a domicile</strong>" de M. <strong>Nossair SEDKI</strong>.</p>
-                                            </div>
-                                        </div>
-                                        <div class="row" style="border-radius:75px">
-                                            <div class="card">
-                                                <div class="header p-6" >
-                                                    <h2>Comments</h2>
+                                        <div class="col">
+                                            <div class="row d-flex flex-row justify-content-center align-items-center align-content-center align-self-center" style="height: 80px;background: #3c3c3c;border-radius: 75px;width: 104%;margin-left: -2%;">
+                                                <div class="col-xl-1" style="max-width: 80px;min-width: 80px;"><img width="100" height="80" src="assets/img/office-front.jpeg" style="height: 60px;width: 60px;border-radius: 60px;margin-left: -1px;"></div>
+                                                <div class="col" style="margin-top: auto;margin-bottom: auto;">
+                                                    <p style="font-size: 15px;font-family: Poppins;margin-top: auto;margin-bottom: auto;color: var(--bs-body-bg);"> <strong>{{ $demandedService->client->name }}</strong> (<i>#Client</i>)  -&gt;&nbsp;<strong>{{ $demandedService->partenaire->name }}</strong> (<i>#Partener</i>)</p>
                                                 </div>
-                                                <div class="body">
-                                                    <ul class="comment-reply list-unstyled">
-                                                        <li class="row clearfix" style="border-radius: 25px;padding: 25px;">
-                                                            <div class="icon-box col-md-2 col-4" style="display: flex;flex-direction: row;justify-content: center;align-items: center">
-                                                                <img class="img-fluid img-thumbnail" style="border-radius: 50%;height: 70px" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Awesome Image">
-                                                            </div>
-                                                            <div class="text-box col-md-7 col-8 p-l-0 p-r0"style="height: 80px">
-                                                                <h5 class="m-b-0" style="font-size: 15px">Gigi Hadid </h5>
-                                                                <p style="font-size: 10px">Why are there so many tutorials on how to decouple WordPress? how fast and easy it is to get it running (and keep it running!) and its massive ecosystem. </p>
-                                                                <ul class="list-inline">
-                                                                    <li><a style="font-size: 10px" href="javascript:void(0);">Mar 09 2018</a></li>
-                                                                </ul>
-                                                            </div>
-                                                        </li>
-                                                        <li class="row clearfix" style="border-radius: 25px;padding: 25px;">
-                                                            <div class="icon-box col-md-2 col-4" style="display: flex;flex-direction: row;justify-content: center;align-items: center">
-                                                                <img class="img-fluid img-thumbnail" style="border-radius: 50%;height: 70px" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Awesome Image">
-                                                            </div>
-                                                            <div class="text-box col-md-7 col-8 p-l-0 p-r0"style="height: 80px">
-                                                                <h5 class="m-b-0" style="font-size: 15px">Gigi Hadid </h5>
-                                                                <p style="font-size: 10px">Why are there so many tutorials on how to decouple WordPress? how fast and easy it is to get it running (and keep it running!) and its massive ecosystem. </p>
-                                                                <ul class="list-inline">
-                                                                    <li><a style="font-size: 10px" href="javascript:void(0);">Mar 09 2018</a></li>
-                                                                </ul>
-                                                            </div>
-                                                        </li><li class="row clearfix" style="border-radius: 25px;padding: 25px;">
-                                                            <div class="icon-box col-md-2 col-4" style="display: flex;flex-direction: row;justify-content: center;align-items: center">
-                                                                <img class="img-fluid img-thumbnail" style="border-radius: 50%;height: 70px" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Awesome Image">
-                                                            </div>
-                                                            <div class="text-box col-md-7 col-8 p-l-0 p-r0"style="height: 80px">
-                                                                <h5 class="m-b-0" style="font-size: 15px">Gigi Hadid </h5>
-                                                                <p style="font-size: 10px">Why are there so many tutorials on how to decouple WordPress? how fast and easy it is to get it running (and keep it running!) and its massive ecosystem. </p>
-                                                                <ul class="list-inline">
-                                                                    <li><a style="font-size: 10px" href="javascript:void(0);">Mar 09 2018</a></li>
-                                                                </ul>
-                                                            </div>
-                                                        </li>
-                                                    </ul>
+                                            </div>
+                                            <div class="row" style="width: auto;margin-right: 0;margin-left: 0;">
+                                                <div class="col">
+                                                    <p style="text-align: left;font-size: 25px;height: auto;width: auto;padding: 43px;">M. <strong>{{ $demandedService->client->name }}</strong>&nbsp; a demander le service de "<strong>{{ $demandedService->service->nom }}</strong>" de M. <strong>{{ $demandedService->partenaire->name }}</strong>.</p>
+                                                </div>
+                                            </div>
+                                            <div class="row" style="border-radius:75px">
+                                                <div class="card">
+                                                    <div class="header p-6" >
+                                                        <h2>Comments</h2>
+                                                    </div>
+                                                    <div class="body">
+                                                        <ul class="comment-reply list-unstyled">
+                                                            @foreach($demandedService->commentaires as $comment)
+                                                                <li class="row clearfix" style="border-radius: 25px;padding: 25px;">
+                                                                    <div class="icon-box col-md-2 col-4" style="display: flex;flex-direction: row;justify-content: center;align-items: center">
+                                                                        <img class="img-fluid img-thumbnail" style="border-radius: 50%;height: 70px" src="https://bootdey.com/img/Content/avatar/avatar7.png" >
+                                                                    </div>
+                                                                    <div class="text-box col-md-7 col-8 p-l-0 p-r0"style="height: 80px">
+                                                                        <h5 class="m-b-0" style="font-size: 20px"> <strong> Anonyme</strong></h5>
+                                                                        <p style="font-size: 25px">{{ $comment->content }}</p>
+                                                                        <ul class="list-inline"style="float: right">
+                                                                            <li><a style="font-size: 15px;text-align: right" href="">{{ $comment->created_at->format('M d Y') }}</a></li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </li>
+                                                                <hr>
+
+                                                            @endforeach
+                                                        </ul>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
+
                                     </div>
-
-                                </div>
+                                @endforeach
                             </div>
                             <div class="col">
                                 <div class="title-div" style="width: auto;margin-top: 30px;margin-right: auto;margin-left: auto;margin-bottom: 30px;text-align: center;">
                                     <h1 style="text-align: center;">Derniers services</h1>
                                 </div>
                                 @foreach($providedServices as $providedService)
-                                    <div style="height: auto;background: var(--bs-light-border-subtle);margin-right: auto;margin-left: auto;max-width: 899px;border-radius: 38px;">
+                                    <div style="height: auto;background: var(--bs-light-border-subtle);margin-right: auto;margin-left: auto;max-width:  600px;border-radius: 38px;">
                                         <div class="col">
                                             <div class="row d-flex flex-row justify-content-center align-items-center align-content-center align-self-center" style="height: 80px;background: #3c3c3c;border-radius: 75px;width: 104%;margin-left: -2%;">
                                                 <div class="col-xl-1" style="max-width: 80px;min-width: 80px;">
